32072c2cbecc5a36e409b8576f6c4bd0e61af056
Galen Heninger <heninger.galen@gmail.com>
2021-01-30 15:45:19 -0900

    Include Stopwatch class

---
+++ CS-202-HW1/stopwatch.cpp
@@ -0,0 +1,25 @@
+//
+//  stopwatch.cpp
+//  CS-202-HW1
+//
+//  Created by Galen on 1/30/21.
+//  Copyright © 2021 Galen. All rights reserved.
+//
+
+#include <chrono>
+#include <ctime>
+
+using std::chrono::system_clock;
+
+#include "stopwatch.hpp"
+
+class Stopwatch {
+    
+
+    
+};
+
+
+
+
+
---
+++ CS-202-HW1/stopwatch.hpp
@@ -0,0 +1,14 @@
+//
+//  stopwatch.hpp
+//  CS-202-HW1
+//
+//  Created by Galen on 1/30/21.
+//  Copyright © 2021 Galen. All rights reserved.
+//
+
+#ifndef stopwatch_hpp
+#define stopwatch_hpp
+
+#include <stdio.h>
+
+#endif /* stopwatch_hpp */
