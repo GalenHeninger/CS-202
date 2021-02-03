8cf9ff7d5cf2e4bb659c482211397e6c57df894d
Galen Heninger <heninger.galen@gmail.com>
2021-02-03 13:58:53 -0900

    Random number generation for search test

--- CS-202-HW1/main.cpp
+++ CS-202-HW1/main.cpp
@@ -7,25 +7,25 @@
 //
 
 #include <iostream>
-
-#include "stopwatch.hpp"
-
 #include <chrono>
 #include <ctime>
+#include <algorithm>
+#include <random>
 
 using std::cout;
 using std::chrono::system_clock;
 using std::ctime;
+using std::search;
 
 class Stopwatch {
     
 public:
     
-    Stopwatch() : _startTime{system_clock::now()},
+    Stopwatch () : _startTime{system_clock::now()},
                   _endTime{system_clock::now()}
     {}
 
-    void start () {
+    void start ()  {
         _startTime = system_clock::now();
     }
 
@@ -33,8 +33,12 @@ public:
         _endTime = system_clock::now();
     }
 
-    double getDuration () {
+    double getSeconds () const {
         return std::chrono::duration<double>(_endTime - _startTime).count();
+    }
+    
+    double getMilliseconds () const {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(_endTime - _startTime).count();
     }
 
 private:
@@ -44,9 +48,39 @@ private:
     
 };
 
+
 int main() {
     
-    Stopwatch A;
+    // Create distribution for RNG
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::uniform_int_distribution<int> distribution(0, 1);
+    
+    // Create vector
+    std::vector<int> v(1000);
+    
+    // Generate
+    auto gen = [&distribution, &generator](){
+        return distribution(generator);
+    };
+    
+    std::generate(v.begin(), v.end(), gen);
+    
+    int heads = std::accumulate(v.begin(), v.end(), 0);
+    
+    std::cout << "Heads: " << heads << "\nTails: " << 1000 - heads << std::endl;
+    
+    
+    Stopwatch timer;
+    
+    // Test search
+    timer.start();
+    
+    timer.stop();
+    
+    double seconds = timer.getSeconds();
+    
+    cout << seconds << "\n";
     
     return 0;
 }
